7d847572870a4dbe5d418f6ebd0208e1c35f611a
Seoyeon Mun <seoyeonmun@Seoyeons-MacBook-Pro.local>
2025-04-06 14:12:46 +0900

    changed config files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'https://aboutexo04.github.io/week10/',
+  base: 'https://aboutexo04.github.io/week10_1/',
   plugins: [react()]
 })
